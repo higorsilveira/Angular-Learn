8dad74f224f0580dc195b1e1d57879f7fc0d76a7
higorsilveira <higorsilveira.hs@gmail.com>
2022-08-24 23:05:49 -0300

    Utilização de variáveis e formsmodule

--- MeuPrimeiroProjeto/src/app/nova-transferencia/nova-transferencia.component.ts
+++ MeuPrimeiroProjeto/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-nova-transferencia',
-  templateUrl:'./nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.sass']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.sass'],
 })
+export class NovaTransferenciaComponent {
 
-export class NovaTransferenciaComponent{
+  valor: number | undefined;
+  destino: number | undefined;
 
+  transferir() {
+    console.log('Solicitada nova transferências');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+  }
 }
